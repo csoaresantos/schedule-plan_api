f8d0622be0ac7811844e4c103692b21635e35dcd
Charles S <charlessoaresdossantos@gmail.com>
2021-01-24 21:38:22 -0300

    implementation of endpoint responsible for retrieve attendee data and update attendee status

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ require('./services/attendee-service');
 // const dataSource = require('./config/database');
 // const server = require('./config/server');
 // asdasdasd asd
-//s asd qwe asd asd asd asd    qwe
-
+//s asd qwe asd asd asd asd    
 //tesdasd asd asd asd asd asdas
 // customerService.end((err) => {
 //     if(err) {
--- services/attendee-service.js
+++ services/attendee-service.js
@@ -7,7 +7,7 @@ const server = require('../config/server');
 server.get('/attendees/:scheduler', function (req, res) {
     const scheduler = req.params.scheduler;
     console.log(`Retriving data from attendees for user ${scheduler}`);
-    dataSource.query(`SELECT a.* FROM attendee a INNER JOIN system_user su ON a.scheduler_user_id = su.user_id WHERE su.user_name = '${scheduler}'`, (err, rows) => {
+    dataSource.query(`SELECT a.* FROM attendee a INNER JOIN system_user su ON a.scheduler_user_id = su.user_id WHERE su.user_name = '${scheduler}' AND a.scheduling_accepted IS NULL`, (err, rows) => {
         if (err) throw err;
 
         res.send(rows);
@@ -16,19 +16,26 @@ server.get('/attendees/:scheduler', function (req, res) {
 });
 
 // This responds a POST request for the homepage
-server.post('/attendees/confirm', function (req, res) {
-    console.log("Updating attendee status");
+server.post('/attendees/confirm', async function (req, res) {
+    console.log("Updating attendee status"+ JSON.stringify(req.body));
 
     const { observation, scheduled_at, scheduling_accepted, attendee_name } = req.body;
 
-    dataSource.query('UPDATE attendee SET observation = ?, scheduled_at = ?, scheduling_accepted = ? WHERE name = ?',
+    await dataSource.query('UPDATE attendee SET observation = ?, scheduled_at = ?, scheduling_accepted = ? WHERE name = ?',
         [observation, scheduled_at, scheduling_accepted, attendee_name],
         (err, result) => {
-            if (err) throw err;
+            if (err) {
+                console.log(err);
+                res.status(400).send({ 'errorMessage': 'Erro ao atualizar dados do cliente!' });
+                
+
+            } else {
+                res.status(200).send({ 'successMessage': 'Dados atualizado com sucess!'});
+            }
 
             console.log('Changed ', result);
         }
     );
 
-    res.send('Hello POST');
+    //res.send('Hello POST');
 });
