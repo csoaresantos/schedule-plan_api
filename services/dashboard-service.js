90f6b619250248c470b1c31747f3711a171463a0
Charles S <charlessoaresdossantos@gmail.com>
2021-06-29 18:43:41 -0300

    adjusts on dashboard

--- services/dashboard-service.js
+++ services/dashboard-service.js
@@ -2,8 +2,8 @@ const server = require('../config/server');
 
 server.get('/dashboard', function (req, res) {
     let list = {
-        'schedulers': ['Eliene V', 'Sheila S', 'Ana C', 'Paula R', 'Maria J', 'Paulo B'],
-        'total_schedulers': [12, 19, 3, 5, 2, 3]
+        'schedulers': ['Eliene V', 'Charles S', 'Ana C', 'Maria J', 'Paulo B'],
+        'total_schedulers': [12, 1, 3, 5, 3]
     };
 
     res.send(list);
